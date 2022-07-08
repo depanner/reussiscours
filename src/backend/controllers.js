6b3879ef0455a1d7750cb66a368c2f4bc806f31a
madrigale <madrigale@reussiscours.fr>
2022-07-08 17:04:17 +0200

    nodemailer added to send emails from backend

--- src/backend/controllers.js
+++ src/backend/controllers.js
@@ -1,4 +1,17 @@
+const nodemailer = require('nodemailer')
+
 const controllers = {}
+
+const transporter = nodemailer.createTransport(
+{         
+    host: '127.0.0.1',
+    port: 25,
+    secure: false,       
+    tls:
+    {
+        rejectUnauthorized: false
+    }    
+})
 
 controllers.email = (req, res) =>
 {
@@ -7,9 +20,31 @@ controllers.email = (req, res) =>
         return res.status(400).send({message: 'bad request'})
     }
 
-    console.log(req.body.name, req.body.email, req.body.phone, req.body.message)
+    const email =
+    {
+        from: 'noreply@reussiscours.fr',
+        to: process.env.EMAIL,
+        subject: `REUSSISCOURS de ${req.body.name}`,
+        html:
+        `
+            <p>Nom Prenom: ${req.body.name}</p>
+            <p>Email: ${req.body.email}</p>
+            <p>Telephone: ${req.body.phone}</p>
+            <p>${req.body.message}</p>
+        `
+    }
 
-    return res.status(200).send({message: 'message sent'})
+    transporter.sendMail(email, (err, info) =>
+    {
+        if(err)
+        {
+            return res.status(500).send({message: 'internal error'})
+        }
+        else
+        {
+            return res.status(200).send({message: 'message sent'})
+        }
+    })
 }
 
 module.exports = controllers
